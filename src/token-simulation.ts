6fc9ab8784ce889556f0f98f74ac8ea6d58f27c4
cybercivizen <khalil.eljouadi@hotmail.com>
2024-02-26 21:05:57 +0100

    added steps

---
+++ src/token-simulation.ts
@@ -0,0 +1,17 @@
+/*
+    I)      Mint new token
+    II)     Transfer tokens
+    III)    Approve delegation / Revoke delegation
+    IV)     Burn tokens  
+
+    I) 
+        1. Create mint account
+        2. Create token associated account
+        3. Mint token
+
+    II)
+        1. Create token associated account
+        2. Transfer tokens
+*/
+
+
